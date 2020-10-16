2d94d9c9538f131967aa18b5af831cdf0d560b4c
ItsmeLee <arsmagica90@gmail.com>
2020-10-16 17:36:09 +0900

    Get infomation from the api url
    The function getJson gets location information from api url.
    api url stores location information.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+const showText1 = document.querySelector('h1');
+const showText2 = document.querySelector('h2');
+const getInfoBtn = document.querySelector('button');
 const API_URL = "http://ip-api.com/json/";
+
+const theRequest = new Request(API_URL);
+
+const getJson = () => {
+    fetch(theRequest,{
+        method : 'GET',
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
+
+
+
+const init = () => {
+    getInfoBtn.addEventListener('click',getJson);
+}
+
+init();
